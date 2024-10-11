c6f3bef98a5573d8ec9e9623c5f8f8bd00a5f04f
Abdullah Zia <amz1cflp99@gmail.com>
2024-10-11 19:37:08 -0400

    Implemented the countOnly function with test code

---
+++ countOnly.js
@@ -0,0 +1,61 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function (allItems, itemstoCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemstoCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results [item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+  // TEST CODE countOnly
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+const results = {};
+return results;
+
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// Some more test cases
+assertEqual("Hello", "Hello");
+assertEqual("Hello", "hello");
+assertEqual(5, 5);
+assertEqual(5, 10);
