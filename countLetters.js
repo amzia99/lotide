cb0d143669f67a839a1ba4c5bad81532ad81afe1
Abdullah Zia <amz1cflp99@gmail.com>
2024-10-11 19:55:30 -0400

    Implemented the countLetters function with test case

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const results = {};
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+// TEST CODE for countLetters
+console.log(countLetters("LHL"))
+console.log(countLetters("Hello World"));
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// Some more test cases
+assertEqual("Hello", "Hello");
+assertEqual("Hello", "hello");
+assertEqual(5, 5);
+assertEqual(5, 10);
