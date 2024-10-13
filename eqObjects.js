c2a14702ecaeabfe039acb4897045d23fb924a25
Abdullah Zia <amz1cflp99@gmail.com>
2024-10-12 20:32:02 -0400

    Implemented the eqObjects function with test cases

---
+++ eqObjects.js
@@ -0,0 +1,45 @@
+// assertEqual code
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// eqObjects code
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// TEST code for eqObjects
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// Some more test cases
+assertEqual("Hello", "Hello");
+assertEqual("Hello", "hello");
+assertEqual(5, 5);
+assertEqual(5, 10);
