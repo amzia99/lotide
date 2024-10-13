0ff0b6d2a255280cd82d8a80be87004030838310
Abdullah Zia <amz1cflp99@gmail.com>
2024-10-13 19:16:22 -0400

    Implemented findKey function with test cases

---
+++ findKey.js
@@ -0,0 +1,32 @@
+// assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// findKey code
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// findKey test cases
+const restaurants = {
+  "Blue Hill": { stars: 1},
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 },
+};
+
+assertEqual(findKey(restaurants, (x) => x.stars === 2), "noma");
+assertEqual(findKey(restaurants, (x) => x.stars === 3), "Akaleri");
+assertEqual(findKey(restaurants, (x) => x.stars === 4), "undefined");
