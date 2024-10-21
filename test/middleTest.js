9a1f32452d570a561322ce05a4565d99f4940cd8
Abdullah Zia <amz1cflp99@gmail.com>
2024-10-20 20:31:20 -0400

    Implemented the refactored middle function with test cases

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+// middleTest code
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+// middleTest test code
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
