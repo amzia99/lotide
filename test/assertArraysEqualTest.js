649cf92b4e5466b3d45b5743b3ca08999413adcd
Abdullah Zia <amz1cflp99@gmail.com>
2024-10-20 20:12:52 -0400

    implemented the assertArraysEqual refactored function with test cases

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+// assertArraysEqual code
+const assertArraysEqual = require('../assertArraysEqual');
+
+// assertArraysEqual test code
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
