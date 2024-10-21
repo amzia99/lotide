fb7d8fc8b7f593bb03b14400c85404d2fcd4d347
Abdullah Zia <amz1cflp99@gmail.com>
2024-10-20 20:15:37 -0400

    fixed errors

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -4,3 +4,5 @@ const assertArraysEqual = require('../assertArraysEqual');
 // assertArraysEqual test code
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
+
+module.exports = assertArraysEqual;
